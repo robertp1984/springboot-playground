b712d6873e1594de1e3d1afa05dc811ae6d96102
robertp1984 <piasecki.robert@gmail.com>
2026-05-03 05:35:37 +0200

    Improved sticky note validation when calling the REST endpoint. Annotations. Closes #8

--- springboot-note/src/main/java/org/softwarecave/springbootnote/config/web/WebControllerAdvice.java
+++ springboot-note/src/main/java/org/softwarecave/springbootnote/config/web/WebControllerAdvice.java
@@ -3,13 +3,13 @@ package org.softwarecave.springbootnote.config.web;
 import org.softwarecave.springbootnote.note.model.NoSuchStickyNoteException;
 import org.softwarecave.springbootnote.note.model.StickyNoteValidationException;
 import org.softwarecave.springbootnote.tag.model.NoSuchTagException;
+import org.softwarecave.springbootnote.tag.model.TagValidationException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
@@ -17,7 +17,7 @@ public class WebControllerAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
-        return ResponseEntity.badRequest().contentType(MediaType.TEXT_PLAIN).body(ex.getMessage());
+        return ResponseEntity.badRequest().contentType(MediaType.TEXT_PLAIN).body("Bad request");
     }
 
     @ExceptionHandler
@@ -27,7 +27,7 @@ public class WebControllerAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler
     public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("Data integrity violation");
     }
 
     @ExceptionHandler
@@ -37,7 +37,12 @@ public class WebControllerAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler
     public ResponseEntity<String> handleStickyNoteValidationException(StickyNoteValidationException ex) {
-        return ResponseEntity.badRequest().contentType(MediaType.TEXT_PLAIN).body(ex.getMessage());
+        return ResponseEntity.badRequest().contentType(MediaType.TEXT_PLAIN).body("Bad sticky note request");
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleTagValidationException(TagValidationException ex) {
+        return ResponseEntity.badRequest().contentType(MediaType.TEXT_PLAIN).body("Bad tag request");
     }
 
 }
